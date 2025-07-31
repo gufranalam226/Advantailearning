8ee6ec9cfca473269f2d637bba9ef49a2f378022
Gufran <gufranalam226@gmail.com>
2025-07-31 10:27:51 +0530

    Bug fixes

--- Project1-Linear Landing page/script.js
+++ Project1-Linear Landing page/script.js
@@ -23,7 +23,7 @@ const image = document.querySelector(".image")
 collDocs.addEventListener("click", ()=>{
     collDocs.style.color = "#ffffff"
     inlineComment.style.color = "#8a8f98"
-    // textIssue.style.color = "#8a8f98"
+    textIssue.style.color = "#8a8f98"
     image.src = "./images/ideate_image1.png"
 
 
@@ -45,5 +45,29 @@ textIssue.addEventListener("click", ()=>{
     textIssue.style.color = "#ffffff"
     image.src = "./images/ideate_image3.png"
 
+
+})
+
+
+const back = document.querySelector(".back")
+const next = document.querySelector(".next")
+const toolcard = document.querySelector('.tool-cards')
+
+let a= 0
+back.addEventListener('click', ()=>{
+    if(a >= 0){
+        a =0
+    }
+    else
+    a+=336
+    toolcard.style.translate = `${a}px`
+})
+next.addEventListener('click', ()=>{
+    if(a<=-1008){
+        a =0;
+    }
+    else 
+    a -= 336
+    toolcard.style.translate = `${a}px`
 
 })
